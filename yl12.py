5b7ccb4771a7136dd828815ffae7b54e13217959
jasperju <jasperjakob.uisk@ametikool.ee>
2024-11-27 15:44:47 +0200

    sumchanges

--- yl12.py
+++ yl12.py
@@ -1,1 +1,36 @@
+#Anna muutuja väärtuseks list kolmest oma lemmik puuviljast ja väljasta see
+#Väljasta listi esimene väärtus
+#Lisa listi lõppu uus puuvili
+#Väljasta listi viimane väärtus
+#Muuda ühe elemendi väärtust ja väljasta kogu list
+#Kontrolli kas väärtus (näiteks õun) eksisteerib listis
+#Väljasta listi pikkus
+#Eemalda listist element ja väljasta kogu list
+#Muuda listi järjekord vastupidiseks
+#Sorteeri list ja väljasta
+
 mylist = ["watermelon", "strawberry", "apelsiin"]
+print(mylist[0])
+
+mylist.append("mango")
+print(mylist[3])
+
+mylist[1] = "banana"
+print(mylist)
+
+if "apple" in mylist:
+    print("apple exists")
+
+else:
+    print("apple doesn't exist")
+
+print(len(mylist))
+
+mylist.remove("mango")
+print(mylist)
+
+mylist.reverse()
+print(mylist)
+
+mylist.sort()
+print(mylist)
